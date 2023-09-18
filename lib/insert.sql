846a0934e5ebdcbb519bb0accb34636c77f04ce2
Anthony Beltran <beltrananthony30@icloud.com>
2023-09-18 14:04:03 -0700

    complete

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1,"Mr. Chocolate", 11, "M", "dark brown", "friendly", 1);
+INSERT INTO bears VALUES (2,"Rowdy", 16, "M", "black", "passive", 1);
+INSERT INTO bears VALUES (3,"Tabitha", 3, "F", "dark brown", "aggressive", 0);
+INSERT INTO bears VALUES (4,"Sergeant Brown", 23, "M", "Green", "playful", 1);
+INSERT INTO bears VALUES (5,"Melissa", 12, "F", "dark brown", "greedy", 1);
+INSERT INTO bears VALUES (6,"Grinch", 1, "M", "Black", "curious", 1);
+INSERT INTO bears VALUES (7,"Wendy", 17, "F", "Blue", "oblivious", 0);
+INSERT INTO bears VALUES (8,null, 21, "M", "black", "lovely", 0);
